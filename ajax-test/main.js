3b6131cf07a5af22feee399ba4a7afd09a1ff3d3
sophieboyle1 <sophieboylemiami@gmail.com>
2020-12-16 12:01:12 +0000

    add table rows and headers to main.js

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -13,9 +13,34 @@ function getData(type, cb) {
     xhr.send();
 }
 
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
 function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
     getData(type, function(data) {
-        console.dir(data);
-        document.getElementById("data").innerHTML = data;
+        var tableRows = [];
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                dataRow.push(`<td>${item[key]}</td>`);
+            });
+            tableRows.push(dataRow)
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
     });
 }
